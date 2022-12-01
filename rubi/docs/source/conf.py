352a8dce69f7be8344742f289283a3e8c9e3c071
denver <denverbaumgartner@gmail.com>
2022-12-01 16:29:46 -0500

    docs

--- rubi/docs/source/conf.py
+++ rubi/docs/source/conf.py
@@ -34,5 +34,6 @@ exclude_patterns = [".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+html_static_path = []
