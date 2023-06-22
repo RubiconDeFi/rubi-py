bcb1e3e49483e63945a0a30598efd61737c2046e
ishandhanani <ishandhanani@gmail.com>
2023-06-22 09:29:27 -0500

    changing test commands to poetry

---
+++ rubi/scripts.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+def test(args=sys.argv):
+    os.chdir("tests")
+    selector_flag = args[1]
+    test_name = args[2]
+    os.system(f"pytest rubi_tests.py -v {selector_flag} {test_name}")
+
+def test_with_coverage():
+    os.chdir("tests")
+    os.system("coverage run -m pytest -v rubi_tests.py && coverage report -m")
+
+def test_coverage_html():
+    os.chdir("tests")
+    os.system("coverage run -m pytest -v rubi_tests.py && coverage html && open htmlcov/index.html") 
