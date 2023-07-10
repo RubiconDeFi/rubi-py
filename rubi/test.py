876b2db2f53c9722b5e0afe32c61ac2abbfa1d2d
ishandhanani <ishandhanani@gmail.com>
2023-07-10 13:18:16 -0500

    removing pre commit and testing black action

---
+++ rubi/test.py
@@ -0,0 +1,2 @@
+def from_http_node_url(cls,http_node_url: str,custom_token_addresses_file: Optional[str] = None,message_queue: Optional[Queue] = None,wallet: Optional[Union[ChecksumAddress, str]] = None,key: Optional[str] = None):
+    return 1
