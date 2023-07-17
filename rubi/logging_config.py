5088dfbade6fc53ce6bce284e9d2e20d855a57f7
ishandhanani <ishandhanani@gmail.com>
2023-07-17 16:04:35 -0500

    black

--- rubi/logging_config.py
+++ rubi/logging_config.py
@@ -7,7 +7,7 @@ log_stream = StringIO()
 # Configure logging to use the in-memory stream
 log_handler = logging.StreamHandler(log_stream)
 log_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 log_handler.setFormatter(formatter)
 
 # Configure logging for the root logger
@@ -15,6 +15,7 @@ root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
 root_logger.addHandler(log_handler)
 
+
 # Function to retrieve the captured logs
 def get_logs():
-    return log_stream.getvalue().strip().split('\n')
+    return log_stream.getvalue().strip().split("\n")
