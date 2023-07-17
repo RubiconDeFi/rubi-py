2697486a4ea37662e43a5f762c9fe27e63a2041f
ishandhanani <ishandhanani@gmail.com>
2023-07-17 13:31:21 -0500

    black

--- rubi/rubi/logging/app.py
+++ rubi/rubi/logging/app.py
@@ -10,19 +10,21 @@ log_stream = StringIO()
 # Configure logging to use the in-memory stream
 log_handler = logging.StreamHandler(log_stream)
 log_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 log_handler.setFormatter(formatter)
 
 # Create a logger for the Flask application and add the log handler
 logger = app.logger
 logger.addHandler(log_handler)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     # Get the logs from the in-memory stream
-    logs = log_stream.getvalue().strip().split('\n')
+    logs = log_stream.getvalue().strip().split("\n")
 
-    return render_template('index.html', logs=logs)
+    return render_template("index.html", logs=logs)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
