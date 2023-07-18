335ff17219516fe2d6146cbf6e4bb461c117811b
ishandhanani <ishandhanani@gmail.com>
2023-07-18 11:55:23 -0500

    new logging config

--- rubi/logging_config.py
+++ rubi/logging_config.py
@@ -1,21 +1,37 @@
 import logging
 from io import StringIO
+import requests
 
 # Create an in-memory stream to store logs
 log_stream = StringIO()
 
-# Configure logging to use the in-memory stream
-log_handler = logging.StreamHandler(log_stream)
-log_handler.setLevel(logging.DEBUG)
+# flag to enable API endpoint
+api_enabled = True
+
+
+class APILogHandler(logging.Handler):
+    def emit(self, record):
+        if api_enabled:
+            log_entry = self.format(record)
+            # send log to flask
+            url = "http://localhost:5000/api/logs"
+            requests.post(url, json={"logs": [log_entry]})
+
+
+# Configure logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+api_log_handler = APILogHandler()
+api_log_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-log_handler.setFormatter(formatter)
+api_log_handler.setFormatter(formatter)
 
-# Configure logging for the root logger
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG)
-root_logger.addHandler(log_handler)
+logger.addHandler(api_log_handler)
 
-
-# Function to retrieve the captured logs
-def get_logs():
-    return log_stream.getvalue().strip().split("\n")
+# # Function to retrieve the captured logs
+# def get_logs():
+#     logs = log_stream.getvalue().strip().split("\n")
+#     log_stream.seek(0)
+#     log_stream.truncate()
+#     return logs
--- rubi/rubi/log_test.py
+++ rubi/rubi/log_test.py
@@ -1,22 +1,7 @@
-import logging
-import requests
-from logging_config import get_logs
-
-logger = logging.getLogger(__name__)
-
-# Configure the logger
-logging.basicConfig(level=logging.DEBUG)
+from logging_config import logger
 
 # Log some events
 logger.debug("Debug message")
 logger.info("Informational message")
 logger.warning("Warning message")
 logger.error("Error message")
-
-# Retrieve the logs
-logs = get_logs()
-
-# Send logs to the Flask app API endpoint
-url = "http://localhost:5000/api/logs"  # Update the URL if necessary
-response = requests.post(url, json={"logs": logs})
-print(response.text)
